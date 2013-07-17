310c668320f1a76bac5d30d87f64d51cb8874b6e
Cédric Messiant <cedricmessiant@ecreall.com>
2013-07-17 14:38:18 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""Installer for the collective.local.sentmail package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+
+setup(
+    name='collective.local.sentmail',
+    version='0.1',
+    description="Keep history of emails sent by collective.local.sendto",
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='email,sendto',
+    author='Cédric Messiant',
+    author_email='cedricmessiant@ecreall.com',
+    url='http://pypi.python.org/pypi/collective.local.sentmail',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['collective', 'collective.local'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'five.grok',
+        'plone.api',
+        'setuptools',
+    ],
+    extras_require={
+        'test': [
+            'ecreall.helpers.testing',
+            'plone.app.testing',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
