cc9ce100cb20a9e19ba368becd43047b25809815
Cédric Messiant <cedricmessiant@ecreall.com>
2013-07-22 14:23:09 +0200

    Add sent_mails_folder_workflow

--- src/collective/local/sentmail/tests/test_security.py
+++ src/collective/local/sentmail/tests/test_security.py
@@ -6,12 +6,20 @@ from ecreall.helpers.testing.workflow import BaseWorkflowTest
 
 from collective.local.sentmail.testing import INTEGRATION, USERDEFS
 
+
 admins = ('admin', 'manager')
 members = ('bart', 'lisa', 'homer', 'marge', 'milhouse')
 SENT_MAIL_PERMISSIONS = {'Access contents information': members + admins,
                          'Modify portal content': ('admin', 'manager'),
                          'View': members + admins,
                          }
+
+everyone = members + admins
+SENT_MAILS_FOLDER_PERMISSIONS = {'Access contents information': everyone,
+                                 'Add portal content': everyone,
+                                 'Modify portal content': admins,
+                                 'View': everyone,
+                                 }
 
 
 class TestSecurity(unittest.TestCase, BaseWorkflowTest):
@@ -25,14 +33,20 @@ class TestSecurity(unittest.TestCase, BaseWorkflowTest):
         self.mails_folder = api.content.create(type="sent_mails_folder",
                                                title=u"Sent mails",
                                                container=self.portal)
+        self.login(creator)
         self.sent_mail = api.content.create(type='sent_mail',
                                             title=u'The email I have sent',
                                             body=u'Hey guys, How are you?',
                                             recipients=['lisa', 'bart'],
                                             container=self.mails_folder)
 
-    def test_permissions_doc(self):
+    def test_permissions_sent_mail(self):
         sent_mail = self.sent_mail
-
         # test sent mail permissions mapping
         self.assertCheckPermissions(sent_mail, SENT_MAIL_PERMISSIONS, USERDEFS)
+
+    def test_permissions_sent_mails_folder(self):
+        mails_folder = self.mails_folder
+        # test sent mails folder permissions mapping
+        self.assertCheckPermissions(mails_folder,
+                                    SENT_MAILS_FOLDER_PERMISSIONS, USERDEFS)
